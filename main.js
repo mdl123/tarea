05749fce3c198f9bbdeb7a463d2f0c7916c5e825
mdl123 <mdlama9@gmail.com>
2014-11-26 21:58:06 -0600

    j

---
+++ main.js
@@ -0,0 +1,8 @@
+$(window).load(function(){
+
+$("#main").css("background-color","lightgrey");
+$("#forma").css("margin","5px opx");
+ $("label").css("float","left");
+ $("label").css("display","block");
+$("#b").css('cursor','hand');
+});
---
+++ main.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Su nombre es: <?php echo $_POST["nombre"]; ?><br>
+Su apellido es: <?php echo $_POST["apellido"]; ?><br>
+Su edad es: <?php echo $_POST["apellido"]; ?><br>
+Su lugar de nacimiento es: <?php echo $_POST["lugarnac"]; ?><br>
+Su e-mail es: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
